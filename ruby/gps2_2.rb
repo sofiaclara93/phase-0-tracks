3fb5651ad8764de1ae3ee9a16b8d0d51840d9afc
Sofia <sofiaclara93@gmail.com>
2016-09-10 10:23:47 -0400

    included update quantity and print method for grocery list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,12 +1,10 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-# ask user for input of list items
 # store as key within hash structure
 # set default quantity
 # ensure all hash contents are lowercase
-# print the list to the console [can you use one of your other methods here?]
-# output: grocery list in hash form
+# output: RETURN grocery list in hash form
 grocery_hash = {}
 
 
@@ -19,16 +17,12 @@ end
   hash 
 end
 
-create_list("carrots apples cereal pizza", grocery_hash)
-
 
 # Method to add an item to a list
-# input: item name and optional quantity
+# input: item name 
 # steps:
-# ask user if they need to add any items
-#   IF yes, add key and value to grocery list hash
-#   ELSE, print list contents
-# output: return the updated hash
+#   add key and value to grocery list hash
+# output: RETURN the updated hash
 
 def add_item(item, hash)
   hash[item] = 1
@@ -36,41 +30,60 @@ def add_item(item, hash)
 end 
 
 
-add_item("banana", grocery_hash)
+
 
 # Method to remove an item from the list
 # input: item that user wants to remove
 # steps:
-# ask user if they want to remove an item from list
-#   IF yes, ask for item name
 #     IF item exists in hash, remove it from hash
-#     ELSE print an error message
-#   ELSE, proceed to output
-# output: return the updated hash
+# output: RETURN the updated hash
 
 def remove_item(item, hash)
   hash.delete(item)
-  p hash
+  hash
 end 
 
-remove_item("banana", grocery_hash)
+
 # Method to update the quantity of an item
 # input: item that user wants to update/updated quantity
 # steps:
-# Ask which item they want to update
-# ask for updated quantity
 #   IF item exists in hash, update the value with user given quantity
-#   ELSE print an error message   
-# output: return the updated hash
+# output: RETURN the updated hash
+
+def update(item, quantity, hash)
+  hash[item] = quantity
+  hash
+end 
 
 
 
 # Method to print a list and make it look pretty
 # input: grocery list hash
 # steps:
-# user asks for list to be printed
 # print value then key for user to view
 # output:
 # print out each key and value of grocery list hash
+
+def print_list(hash)
+  hash.each do |item, quantity| 
+    puts "Don't forget to buy #{quantity} #{item}"
+  end 
+  hash 
+end
+
+
+
+create_list("carrots apples cereal pizza", grocery_hash)
+add_item("Lemonade", grocery_hash)
+add_item("Tomatoes", grocery_hash)
+add_item("Onions", grocery_hash)
+add_item("Ice cream", grocery_hash)
+update("Lemonade", 2, grocery_hash)
+update("Tomatoes", 3, grocery_hash)
+update("Ice cream", 4, grocery_hash)
+update("Ice cream", 1, grocery_hash)
+remove_item("Lemonade", grocery_hash)
+
+print_list(grocery_hash)
 
 
